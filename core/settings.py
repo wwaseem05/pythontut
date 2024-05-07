87660a6bf54d04d9a36906f3558d5f89ed999fca
wwaseem05 <wwaseem@academic.rrc.ca>
2024-05-07 13:49:04 -0500

    new project

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "newapp", # new app 
+    'newapp', # new app 
 ]
 
 MIDDLEWARE = [
